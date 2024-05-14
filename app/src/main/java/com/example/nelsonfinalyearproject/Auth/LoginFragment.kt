3315cc0b144b980d62aaa60327e1693978ade00b
Mriganka Bordoloi <mrigankatiwasa@gmail.com>
2024-05-15 01:29:29 +0530

    google login backend is going on

--- app/src/main/java/com/example/nelsonfinalyearproject/Auth/LoginFragment.kt
+++ app/src/main/java/com/example/nelsonfinalyearproject/Auth/LoginFragment.kt
@@ -1,25 +1,46 @@
 package com.example.nelsonfinalyearproject.Auth
 
+import android.app.Activity
+import android.content.ContentValues
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.IntentSenderRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.nelsonfinalyearproject.MainActivity
 import com.example.nelsonfinalyearproject.R
 import com.example.nelsonfinalyearproject.databinding.FragmentLoginBinding
+import com.google.android.gms.auth.api.identity.BeginSignInRequest
+import com.google.android.gms.auth.api.identity.Identity
+import com.google.android.gms.auth.api.identity.SignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.auth
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
 
 class LoginFragment : Fragment() {
 
     private lateinit var binding: FragmentLoginBinding
     private lateinit var auth: FirebaseAuth
+    private lateinit var googleSignInClient : GoogleSignInClient
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -36,6 +57,27 @@ class LoginFragment : Fragment() {
         auth = Firebase.auth
 
 
+        // Google Auth
+        val gso = GoogleSignInOptions
+            .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(requireContext(),gso)
+
+        binding.ivGoogleIcon.setOnClickListener {
+            val signInClient = googleSignInClient.signInIntent
+            launcher.launch(signInClient)
+
+        }
+
+
+        //
+
+        // Buttons Navigation
+
+
         binding.tvSignup.setOnClickListener {
             findNavController().navigate(R.id.signupFragment)
         }
@@ -47,6 +89,8 @@ class LoginFragment : Fragment() {
         binding.tvForgotPass.setOnClickListener {
             findNavController().navigate(R.id.forgotPasswordFragment)
         }
+
+        ///////
 
 
         binding.btnLogin.setOnClickListener {
@@ -106,5 +150,30 @@ class LoginFragment : Fragment() {
         val intent = Intent(requireContext(), MainActivity::class.java)
         startActivity(intent)
     }
+
+    private val launcher=registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+        result ->
+
+        if(result.resultCode==Activity.RESULT_OK){
+            val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+
+            if(task.isSuccessful){
+                val account: GoogleSignInAccount? = task.result
+                val credential = GoogleAuthProvider.getCredential(account?.idToken,null)
+                auth.signInWithCredential(credential)
+                    .addOnCompleteListener {
+                        if (it.isSuccessful){
+                            Toast.makeText(requireContext(),"Done",Toast.LENGTH_LONG).show()
+
+                        }else{
+                            Toast.makeText(requireContext(),"Failed",Toast.LENGTH_LONG).show()
+                        }
+                    }
+            }
+        }else{
+            Toast.makeText(requireContext(),"Failed",Toast.LENGTH_LONG).show()
+        }
+    }
+
 
 }
