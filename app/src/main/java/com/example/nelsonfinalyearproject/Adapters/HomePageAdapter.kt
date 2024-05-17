7d9a711817be091cc08fd7ee06b799148ef14101
Mriganka Bordoloi <mrigankatiwasa@gmail.com>
2024-05-17 11:51:27 +0530

    working on home fragment

---
+++ app/src/main/java/com/example/nelsonfinalyearproject/Adapters/HomePageAdapter.kt
@@ -0,0 +1,25 @@
+package com.example.nelsonfinalyearproject.Adapters
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.example.nelsonfinalyearproject.databinding.ItemDateTimeBinding
+
+class HomePageAdapter:RecyclerView.Adapter<ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+}
+
+class ViewHolder( val binding: ItemDateTimeBinding) : RecyclerView.ViewHolder(binding.root) {
+
+
+}
